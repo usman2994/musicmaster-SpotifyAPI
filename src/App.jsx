3890a23ccbde87c06216103f26d3ea9b1127dc0a
Usman <usman2994>
2017-08-28 15:37:10 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import './App.css';
+import Profile from './Profile';
+import Gallery from './Gallery';
+import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            query:'',
+            artist: null,
+            tracks: []
+        }
+    }
+
+    search() {
+        console.log('this.state',this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+        var accessToken = 'BQAdq3KHJ2iN7vG10gJx8Jnh3oOc_MdnBtSxK8l6GmfPdQ9SX7eRkkiLhH73Rzkj3uXugm2T8EVskZRbCe7xsHzghSdab9M8nV_rj-mxF-3OR0SW7Lv4QxLRPmgkVJXasP0rElCgxoGVjLC6G_iEa3NWoHVXwmfSIclD73VHewiswPfvtNs';
+        var myHeaders = new Headers();
+
+        var myOptions = {
+            method: 'GET',
+            headers:  {
+               'Authorization': 'Bearer ' + accessToken
+            },
+            mode: 'cors',
+            cache: 'default'
+        };
+
+        fetch(FETCH_URL, myOptions )
+            .then(response => response.json())
+            .then(json => {
+                const artist = json.artists.items[0];
+                console.log('artist', artist);
+                this.setState({artist});
+
+        FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+        fetch(FETCH_URL, myOptions)
+            .then(response => response.json())
+            .then(json => {
+                console.log('artists top tracks: ', json);
+                const { tracks } = json;
+                this.setState({tracks});
+            })
+
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-title">Music master</div>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search for an Artist"
+                            value={this.state.query}
+                            onChange={event => {this.setState({query: event.target.value})}}
+                            onKeyPress={event => {
+                                if (event.key === 'Enter') {
+                                    this.search()
+                                }
+                            }}
+                        />
+                        <InputGroup.Addon onClick={() => this.search()}>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                {
+                    this.state.artist !==null
+                    ?
+                        <div>
+                            <Profile
+                                artist={this.state.artist}
+                            />
+                            <Gallery
+                                tracks={this.state.tracks}
+                            />
+                        </div>
+                    :   <div></div>
+                }
+            </div>
+        )
+    }
+}
+
+export default App;
